ef2572849683e6bdfd75806564e9e83b9be9ccbd
Maria Teresa <mteresa1096@gmail.com>
2019-05-15 19:27:39 +0930

    editor

--- js/script.js
+++ js/script.js
@@ -24,13 +24,15 @@ var span3 = document.getElementById("close3");
 btn3.onclick = function() {
   modal3.style.display = "block";
 }
-span1.onclick = function() {
-  modal1.style.display = "none";
+span3.onclick = function() {
+  modal3.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal1 || event.target == modal2 || event.target == modal3) {
     modal1.style.display = "none";
+    modal2.style.display = "none";
+    modal3.style.display = "none";
   }
 }
