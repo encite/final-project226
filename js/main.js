552ce37a1633244d240075c091e4a3bce32577aa
sujn312 <sujan.khadka312@gmail.com>
2019-05-30 18:31:48 +0930

    7th card added along with css and javascript

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ navBarToggle.addEventListener('click', function () {
 
     mainNav.classList.toggle('active');
 });
+
+// accordion adapted from W3school
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
